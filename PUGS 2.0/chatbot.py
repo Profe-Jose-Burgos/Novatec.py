54539c0a2e61a7adc8e08ed901a791f3132ff430
Rodrigo <89274708+Itsscoconut@users.noreply.github.com>
2023-01-24 18:37:43 -0500

    correccion de ruta

--- PUGS 2.0/chatbot.py
+++ PUGS 2.0/chatbot.py
@@ -6,10 +6,10 @@ from tensorflow.keras.models import load_model
 
 lematizador = SnowballStemmer('spanish')
 
-modelo = load_model("modelo_chatbot_pugs.h5")
-intenciones = json.loads(open("intenciones.json").read())
-palabras = pickle.load(open("palabras.pkl","rb"))
-categorias = pickle.load(open("categorias.pkl","rb"))
+modelo = load_model("Novatec.py/PUGS 2.0/modelo_chatbot_pugs.h5")
+intenciones = json.loads(open("Novatec.py/PUGS 2.0/intenciones.json").read())
+palabras = pickle.load(open("Novatec.py/PUGS 2.0/palabras.pkl","rb"))
+categorias = pickle.load(open("Novatec.py/PUGS 2.0/categorias.pkl","rb"))
 
 
 def limpiar_conversacion(sentence):
